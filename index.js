0c3aeda7083142c3a1dc71d178136ea009c645d0
Ruanderson Vieira <ruanderson@accalia.io>
2020-05-31 19:59:23 -0300

    :sparkles: upload file

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 const { uploadFile } = require('./s3/s3upload');
 
-// console.log(uploadFile);
-
-uploadFile('image/rocket2.jpeg','rocket2.jpeg');
+uploadFile('image/image/jpg', 'filename');
--- s3/s3upload.js
+++ s3/s3upload.js
@@ -1,14 +1,19 @@
+require('dotenv').config()
+
 const AWS = require('aws-sdk');
 const fs = require('fs');
-require('dotenv').config()
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ID,
-  secretAccessKey: process.env.SECRET
+  secretAccessKey: process.env.AWS_SECRET
 });
 
-const uploadFile = (filePath, fileName) => {
+const uploadFile = async (filePath, fileName) => {
   const fileContent = fs.readFileSync(filePath);
+
+  if (!fileName) {
+    return console.error('Not send `fileName` in params');
+  }
 
   const params = {
     Bucket: process.env.AWS_BUCKET_NAME,
@@ -16,11 +21,12 @@ const uploadFile = (filePath, fileName) => {
     Body: fileContent
   };
 
-  s3.upload(params, function(err, data) {
+  const Location = await s3.upload(params, function(err, data) {
       if (err) throw err;
-
       return data.Location;
   });
+
+  return console.log(Location);
 }
 
 exports.uploadFile = uploadFile;
